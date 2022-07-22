3768febafda27180f454768805d8450f4b8457bb
Stacy Oswald <stacy.oswald@stacys-mbp.home>
2022-07-22 16:32:39 +0100

    Remove redundant tests

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -41,76 +41,6 @@ describe "A cell with different states" do
   end
 end
 
-# describe "we've gotten to neighbours" do
-#   before "create the world" do 
-#     @world = Grid.new
-#   end
-
-#   it 'cell 1 can see the state of cell 2' do
-#     expect(@world.neighbour_state_east(0,0)).to eq(@world.grid[0][1].state)
-#   end
-
-#   it 'last cell sees the right border' do
-#     expect(@world.neighbour_state_east(0,5)).to eq("border")
-#   end
-
-#   it 'the second cell sees the first cell' do
-#     expect(@world.neighbour_state_west(0,1)).to eq(@world.grid[0][0].state)
-#   end
-
-#   it 'the top left cell sees the left border' do
-#     expect(@world.neighbour_state_west(0,0)).to eq("border")
-#   end
-
-#   it 'the right hand border exists... ' do
-#     expect(@world.neighbour_state_east(0, -2)).to eq("border")
-#   end
-
-#   it 'the right hand border has not run away yet... ' do
-#     expect(@world.neighbour_state_east(1, -2)).to eq("border")
-#   end
-
-#   it 'the right hand border is really stubborn... ' do
-#     expect(@world.neighbour_state_east(6, -2)).to eq("border")
-#   end
-
-#   it 'the bottom border is on top of the world... ' do
-#     expect(@world.neighbour_state_south(-2, 0)).to eq("border")
-#   end
-  
-  # it 'the cell can see north' do
-  #   expect(@world.neighbour_state_north(2,4)).to eq(@world.grid[1][4].state)
-  # end
-
-  # it 'the cell can see north east' do
-  #   expect(@world.neighbour_state_north_east(1,2)).to eq(@world.grid[0][3].state)
-  # end
-
-  # it 'the cell can see east' do
-  #   expect(@world.neighbour_state_north_east(4,4)).to eq(@world.grid[4][5].state)
-  # end
-
-  # it 'the cell can see south east' do
-  #   expect(@world.neighbour_state_south_east(3,4)).to eq(@world.grid[4][5].state)
-  # end
-  
-  # it 'the cell can see south' do
-  #   expect(@world.neighbour_state_south(4,3)).to eq(@world.grid[5][3].state)
-  # end
-
-  # it 'the cell can see south west' do
-  #   expect(@world.neighbour_state_south_west(2,2)).to eq(@world.grid[3][1].state)
-  # end
-
-  # it 'the cell can see west' do
-  #   expect(@world.neighbour_state_south(5,0)).to eq(@world.grid[5][6].state)
-  # end
-
-  # it 'the cell can see north west' do
-  #   expect(@world.neighbour_state_west(2,3)).to eq(@world.grid[1][2].state)
-  # end
-# end
-
 describe "awaking the cells!" do
   it "can assign a starting cell to be alive" do
     @world = Grid.new
@@ -138,7 +68,6 @@ describe "awaking the cells!" do
 end
 
 describe "number of awake neighbours" do
-
   it "outputs 0 when there are no living cells" do
     @world = Grid.new
     expect(@world.living_neighbour_count(2,2)).to eq(0)
